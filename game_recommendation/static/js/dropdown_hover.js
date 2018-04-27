b0293dcaa8dd460717cfa256fe2893713d057698
dawid <budzynski.dawid@gmail.com>
2018-04-28 00:52:34 +0200

    new dropdown menu

---
+++ game_recommendation/static/js/dropdown_hover.js
@@ -0,0 +1,5 @@
+$('.navbar .dropdown').hover(function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+}, function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+});
