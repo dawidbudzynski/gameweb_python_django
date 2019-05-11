4f0dc5eed8ec5dbc458d7843318de732b4d0176a
Dawid Budzynski <budzynski.dawid@gmail.com>
2019-05-11 20:57:29 +0200

    internalization added

--- final_project_coderslab/urls.py
+++ final_project_coderslab/urls.py
@@ -1,24 +1,12 @@
-"""projekt_koncowy URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-urlpatterns = [
+urlpatterns = []
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include("news_api.urls", namespace="news_api-mainpage")),
     path('developer/', include("developer.urls", namespace="developer")),
@@ -29,7 +17,7 @@ urlpatterns = [
     path('recommendation/', include("game_recommendation.urls", namespace="game_recommendation")),
     path('tag/', include("tag.urls", namespace="tag")),
     path('users/', include("users.urls", namespace="users"))
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
