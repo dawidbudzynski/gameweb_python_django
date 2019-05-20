907f0540dbfa3bd516cde1383070952b8e2f1658
Dawid Budzynski <budzynski.dawid@gmail.com>
2019-05-20 20:53:38 +0200

    quotes slider improved

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
-var limit = 7;
-$('input[name="tags"]').on('change', function(evt) {
-   if($('input[name="tags"]:checked').length >= limit) {
-       this.checked = false;
-   }
+let limit = 7;
+$('input[name="tags"]').on('change', function (evt) {
+    if ($('input[name="tags"]:checked').length >= limit) {
+        this.checked = false;
+    }
 });
 
--- static/js/slider.js
+++ static/js/slider.js
@@ -1,15 +1,16 @@
-var cnt = 0,
-  texts = [];
+let cnt = 0,
+    texts = [];
 
-$(".textContent").each(function() {
-  texts[cnt++] = $(this).html();
+$(".textContent").each(function () {
+    texts[cnt++] = $(this).html();
 });
 
 function slide() {
-  if (cnt >= texts.length) cnt = 0;
-  $('#textMessage').html(texts[cnt++]);
-  $('#textMessage')
-    .fadeIn(3000).delay(3000)
-    .fadeOut(3000, slide);
+    if (cnt >= texts.length) cnt = 0;
+    $('#textMessage')
+        .html(texts[cnt++])
+        .fadeIn(3000).delay(3000)
+        .fadeOut(3000, slide);
 }
+
 slide();
