8cd2dabeb68c6d0d9f1e9eaac9326758e3b07350
Dawid Budzynski <budzynski.dawid@gmail.com>
2019-05-25 20:46:37 +0200

    heroku config

--- final_project_coderslab/settings.py
+++ final_project_coderslab/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
