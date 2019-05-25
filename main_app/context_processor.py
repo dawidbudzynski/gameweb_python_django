35b2046439f6fd5b3336a528e3e378b06020781e
Dawid Budzynski <budzynski.dawid@gmail.com>
2019-05-25 19:50:11 +0200

    minor changes

--- main_app/context_processor.py
+++ main_app/context_processor.py
@@ -8,7 +8,6 @@ def gaming_quotes_processor(request):
     all_quotes = []
     path = os.path.join(settings.MEDIA_ROOT, 'csv/gaming_quotes.csv')
     with open(path) as f:
-        reader = csv.reader(f, delimiter=';')
-        for row in reader:
+        for row in csv.reader(f, delimiter=';'):
             all_quotes.append({'author': row[0], 'quote': row[1]})
     return {'all_quotes': all_quotes}
